03ca72fa38f82274a2e98d3e8db9ab069733ef38
Ubuntu <clint@fewbar.com>
2014-08-13 04:15:08 +0000

    Set instance-id in heat inventory

--- plugins/inventory/heat.py
+++ plugins/inventory/heat.py
@@ -121,6 +121,7 @@ class HeatInventory(object):
                     hostvars[addr] = {'heat_metadata':
                         self.hclient.resources.metadata(
                             stack_id, res.resource_name)}
+                    hostvars[addr]['instance-id'] = res.physical_resource_id
         inventory = {'_meta': {'hostvars': hostvars}}
         inventory.update(groups)
         print(json.dumps(inventory, indent=2))
