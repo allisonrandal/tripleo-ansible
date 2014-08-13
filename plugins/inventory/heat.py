ce5c9d691a0bf09abf6c8c162c758ea638b8fd82
Julia Kreger <juliaashleykreger@gmail.com>
2014-08-13 16:52:20 -0400

    Cleanup heat.py
    Removed un-needed code from the host lookup, and cleaned up the code
    for pep8 errors.

--- plugins/inventory/heat.py
+++ plugins/inventory/heat.py
@@ -22,16 +22,19 @@ import sys
 from oslo.config import cfg
 
 opts = [
-        cfg.StrOpt('host', help='List details about the specific host'),
-        cfg.BoolOpt('list', help='List active hosts'),
-        cfg.MultiStrOpt('stack', help='Stack IDs or Names to inspect',
-            positional=True),
-        cfg.StrOpt('username'),
-        cfg.StrOpt('password'),
-        cfg.StrOpt('auth-url'),
-        cfg.StrOpt('project-id'),
-        cfg.StrOpt('group-regex'),
-        ]
+    cfg.StrOpt('host', help='List details about the specific host'),
+    cfg.BoolOpt('list', help='List active hosts'),
+    cfg.MultiStrOpt(
+        'stack',
+        help='Stack IDs or Names to inspect',
+        positional=True
+    ),
+    cfg.StrOpt('username'),
+    cfg.StrOpt('password'),
+    cfg.StrOpt('auth-url'),
+    cfg.StrOpt('project-id'),
+    cfg.StrOpt('group-regex'),
+]
 
 try:
     from heatclient.v1 import client as heat_client
@@ -69,12 +72,13 @@ def _parse_config():
     if configs.password is None:
         if "OS_PASSWORD" in os.environ:
             configs.password = os.environ.get('OS_PASSWORD')
-    if configs.project_id is None: 
+    if configs.project_id is None:
         if "OS_TENANT_NAME" in os.environ:
             configs.project_id = os.environ.get('OS_TENANT_NAME')
     if '/v2.0' in configs.auth_url:
-        configs.auth_url = configs.auth_url.replace('/v2.0','/v3')
+        configs.auth_url = configs.auth_url.replace('/v2.0', '/v3')
     return configs
+
 
 class HeatInventory(object):
     def __init__(self, configs):
@@ -88,7 +92,9 @@ class HeatInventory(object):
         if not self.configs.stack:
             return self.hclient.stacks.list()
         else:
-            return [ self.hclient.stacks.get(stack) for stack in self.configs.stack ]
+            return [
+                self.hclient.stacks.get(stack) for stack in self.configs.stack
+            ]
 
     def list(self):
         hostvars = {}
@@ -96,21 +102,31 @@ class HeatInventory(object):
         # XXX: need to config access details
         for stack_obj in self.stacks:
             if stack_obj.status != 'COMPLETE':
-                print(stack + " stack is incomplete, in state " + stack_obj.status)
+                print("%s stack is incomplete, in state %s" % (
+                    stack,
+                    stack_obj.status
+                ))
                 sys.exit(1)
             stack_id = stack_obj.id
             for res in self.hclient.resources.list(stack_id):
                 if res.resource_type == 'OS::Nova::Server':
                     server = self.nclient.servers.get(res.physical_resource_id)
                     name = server.name
-                    private = [ x['addr'] for x in getattr(server,
-                                                           'addresses').itervalues().next()
-                               if x['OS-EXT-IPS:type'] == 'fixed']
+                    private = [
+                        x['addr'] for x in getattr(
+                            server,
+                            'addresses'
+                        ).itervalues().next()
+                        if x['OS-EXT-IPS:type'] == 'fixed'
+                    ]
                     if private:
                         private = private[0]
-                    public  = [ x['addr'] for x in getattr(server,
-                                                           'addresses').itervalues().next()
-                               if x['OS-EXT-IPS:type'] == 'floating']
+                    public = [
+                        x['addr'] for x in getattr(
+                            server,
+                            'addresses'
+                        ).itervalues().next()
+                        if x['OS-EXT-IPS:type'] == 'floating']
 
                     if public:
                         public = public[0]
@@ -119,7 +135,8 @@ class HeatInventory(object):
                     groups[server.name] = [addr]
                     if self.configs.group_regex:
                         group_name = re.search(
-                                self.configs.group_regex, res.resource_name)
+                            self.configs.group_regex, res.resource_name
+                        )
                         if group_name:
                             group_name = group_name.group(0)
                             if group_name in groups:
@@ -132,9 +149,12 @@ class HeatInventory(object):
                             groups[group_name].append(addr)
                         else:
                             groups[group_name] = [addr]
-                    hostvars[addr] = {'heat_metadata':
-                        self.hclient.resources.metadata(
-                            stack_id, res.resource_name)}
+                    hostvars[addr] = {
+                        'heat_metadata': self.hclient.resources.metadata(
+                            stack_id,
+                            res.resource_name
+                        )
+                    }
                     hostvars[addr]['instance_id'] = res.physical_resource_id
         inventory = {'_meta': {'hostvars': hostvars}}
         inventory.update(groups)
@@ -146,29 +166,22 @@ class HeatInventory(object):
 
         for stack_obj in self.stacks:
             if stack_obj.status != 'COMPLETE':
-                print(stack + " stack is incomplete, in state " + stack_obj.status)
+                print("%s stack is incomplete, in state %s" % (
+                    stack,
+                    stack_obj.status
+                ))
                 sys.exit(1)
             stack_id = stack_obj.id
             for res in self.hclient.resources.list(stack_id):
                 if res.resource_type == 'OS::Nova::Server':
                     server = self.nclient.servers.get(res.physical_resource_id)
                     if self.configs.host in server.name:
-                        name = server.name
-                        private = [ x['addr'] for x in getattr(server,
-                                                               'addresses').itervalues().next()
-                                   if x['OS-EXT-IPS:type'] == 'fixed']
-                        if private:
-                            private = private[0]
-                        public  = [ x['addr'] for x in getattr(server,
-                                                               'addresses').itervalues().next()
-                                   if x['OS-EXT-IPS:type'] == 'floating']
-
-                        if public:
-                            public = public[0]
-                        addr = server.accessIPv4 or public or private
-                        hostvars = {'heat_metadata':
-                            self.hclient.resources.metadata(
-                                stack_id, res.resource_name)}
+                        hostvars = {
+                            'heat_metadata': self.hclient.resources.metadata(
+                                stack_id,
+                                res.resource_name
+                            )
+                        }
                         hostvars['instance_id'] = res.physical_resource_id
                         print(json.dumps(hostvars, indent=2))
             break
@@ -177,9 +190,9 @@ class HeatInventory(object):
     def ksclient(self):
         if self._ksclient is None:
             self._ksclient = keystone_client.Client(
-                    auth_url=self.configs.auth_url,
-                    username=self.configs.username,
-                    password=self.configs.password)
+                auth_url=self.configs.auth_url,
+                username=self.configs.username,
+                password=self.configs.password)
             self._ksclient.authenticate()
         return self._ksclient
 
@@ -188,10 +201,10 @@ class HeatInventory(object):
         if self._hclient is None:
             ksclient = self.ksclient
             endpoint = ksclient.service_catalog.url_for(
-                    service_type='orchestration', endpoint_type='publicURL')
+                service_type='orchestration', endpoint_type='publicURL')
             self._hclient = heat_client.Client(
-                    endpoint=endpoint,
-                    token=ksclient.auth_token)
+                endpoint=endpoint,
+                token=ksclient.auth_token)
         return self._hclient
 
     @property
@@ -199,14 +212,14 @@ class HeatInventory(object):
         if self._nclient is None:
             ksclient = self.ksclient
             endpoint = ksclient.service_catalog.url_for(
-                    service_type='compute', endpoint_type='publicURL')
+                service_type='compute', endpoint_type='publicURL')
             self._nclient = nova_client.Client(
-                    bypass_url=endpoint,
-                    username=None,
-                    api_key=None,
-                    project_id=self.configs.project_id,
-                    auth_url=self.configs.auth_url,
-                    auth_token=ksclient.auth_token)
+                bypass_url=endpoint,
+                username=None,
+                api_key=None,
+                project_id=self.configs.project_id,
+                auth_url=self.configs.auth_url,
+                auth_token=ksclient.auth_token)
         return self._nclient
 
 
